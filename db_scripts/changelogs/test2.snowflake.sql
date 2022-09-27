7a96efac633ff54163a49108937d534507a7dc1f
Dodda <manish.dodda@pfizer.com>
2022-09-27 16:54:20 +0530

    doddam

--- db_scripts/changelogs/test2.snowflake.sql
+++ db_scripts/changelogs/test2.snowflake.sql
@@ -3,4 +3,4 @@
 -- changeset aayushjain:04
 
 INSERT INTO AUDIT_DTLS_CICD_TESTING4 (BATCH_ID, APPLICATION_NAME, SUBJECT_AREA_NAME, INTERFACE_NAME, TASK_NAME, TASK_TYPE)
-VALUES ('2', 'liquibase', 'integration', 'snowflake', 'intgration', 'normal');
+VALUES ('3', 'liquibase', 'integration', 'snowflake', 'intgration', 'normal');
