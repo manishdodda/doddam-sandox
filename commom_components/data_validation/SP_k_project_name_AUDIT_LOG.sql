f20775bc2f930a51432d79d233db308233d07be2
THAPAA07 <125654283+THAPAA07@users.noreply.github.com>
2024-05-24 16:15:58 +0530

    Add files via upload

---
+++ commom_components/data_validation/SP_k_project_name_AUDIT_LOG.sql
@@ -0,0 +1,169 @@
+CREATE OR REPLACE PROCEDURE k_database_name.k_schema_name.SP_k_project_name_AUDIT_LOG("P_INTERFACE_NM" VARCHAR(200), "P_TASK_NAME" VARCHAR(200), "P_SUB_AREA_NM" VARCHAR(200), "P_TASK_TYPE" VARCHAR(200), "P_EXECUTION_STATUS" VARCHAR(200), "P_EXCEP_ERR" VARCHAR(200))
+RETURNS VARCHAR(16777216)
+LANGUAGE JAVASCRIPT
+EXECUTE AS OWNER
+AS ' 
+try 
+{
+    snowflake.execute({ sqlText: "Begin Transaction;"});
+	
+    var v_return_value="";
+    
+    var V_CONTROL_SCHEMA_NAME = "k_schema_name";
+	
+------Fetching param value from parameter table
+	
+    var V_SQ = `SELECT PARAM_VALUE FROM `+V_CONTROL_SCHEMA_NAME+`.k_project_name_PARAMETER_TABLE WHERE PARAM_NAME = ''CONTROL''`;
+
+    var V_ST = snowflake.createStatement( {sqlText: V_SQ} ).execute();
+
+    V_ST.next();
+
+    var V_DATABASE_SCHEMA_NAME = V_ST.getColumnValue(1);
+	
+    v_return_value += "\\n P_SUB_AREA_NM: " + P_SUB_AREA_NM;
+	
+    v_return_value += "\\n P_INTERFACE_NM: " + P_INTERFACE_NM;
+	
+    v_return_value += "\\n P_TASK_NAME: " + P_TASK_NAME;
+	
+    v_return_value += "\\n P_TASK_TYPE: " + P_TASK_TYPE;
+	
+    v_return_value += "\\n P_EXECUTION_STATUS: " + P_EXECUTION_STATUS;
+	
+    v_return_value += "\\n P_EXCEP_ERR: " + P_EXCEP_ERR;
+	
+-------Fetching active batch id from batch details
+		 
+	var V_FETCH_BATCH_SQ = `SELECT BATCH_ID,APPLICATION_ID FROM `+V_DATABASE_SCHEMA_NAME+`.k_project_name_BATCH_RUN_DTLS WHERE UPPER(SUBJECT_AREA_NAME) = UPPER(''`+ P_SUB_AREA_NM +`'') AND LOAD_STATUS = ''R''`;
+	
+	var V_FETCH_BATCH_ST = snowflake.createStatement( {sqlText: V_FETCH_BATCH_SQ} ).execute();
+
+--------------------If active batch found
+	
+	if(V_FETCH_BATCH_ST.next())
+	{
+		V_BATCH_ID = V_FETCH_BATCH_ST.getColumnValue(1);
+		
+		v_return_value += "\\n V_BATCH_ID: " + V_BATCH_ID;
+		
+		var V_APPLICATION_ID = V_FETCH_BATCH_ST.getColumnValue(2);
+	}
+
+--------If no active batch found
+	
+	else
+	{
+		v_return_value += " NO BATCH IS OPEN FOR THE INTERFACE. OPEN A NEW BATCH ";
+	
+		throw v_return_value;
+	}
+	
+	var V_STAT_DESC="";
+	
+----Marking different Execution status description in V_STAT_DESC
+	
+	if ( P_EXECUTION_STATUS == ''R'' )
+	{ 
+		V_STAT_DESC = "RUNNING";
+	}
+	else if ( P_EXECUTION_STATUS == ''F'' ) 
+	{
+		V_STAT_DESC = "FAILED";
+	}
+	else if ( P_EXECUTION_STATUS == ''C'' )
+	{
+		V_STAT_DESC = "COMPLETE";
+	}
+	else 
+	{
+		V_STAT_DESC="";
+	}
+	
+	v_return_value += "\\n V_STAT_DESC: " + V_STAT_DESC;
+	
+	
+	if ( P_EXECUTION_STATUS == ''R'' )
+	{
+	
+-------Marking current job as F for restartibility purpose
+	
+		var V_UPD_ARCH_RESTRT_DML = `UPDATE `+V_DATABASE_SCHEMA_NAME+`.k_project_name_AUDIT_DTLS SET EXECUTION_END_TIME = CURRENT_TIMESTAMP(),EXECUTION_TIME_SECS = datediff(SECOND, EXECUTION_START_TIME, CURRENT_TIMESTAMP()),EXECUTION_STATUS = ''F'', EXECUTION_STATUS_DESC = ''FAILED'', ERROR_DESC = ''MANUAL FAILURE OVERRIDE'' , LAST_UPDATE_DATE = CURRENT_TIMESTAMP()	WHERE UPPER(SUBJECT_AREA_NAME) = UPPER(''` + P_SUB_AREA_NM +`'') AND UPPER(TASK_NAME) = UPPER(''`+ P_TASK_NAME +`'') AND EXECUTION_STATUS = ''R''`;
+	
+		var V_UPD_ARCH_RESTRT_DML_ST = snowflake.createStatement( {sqlText: V_UPD_ARCH_RESTRT_DML} ).execute();
+
+		var V_FETCH_APPLICATION_NAME_SQ = `SELECT APPLICATION_NAME FROM `+V_DATABASE_SCHEMA_NAME+`.k_project_name_APPLICATION_DTLS WHERE APPLICATION_ID = `+V_APPLICATION_ID+``;
+		
+		var V_FETCH_APPLICATION_NAME_ST = snowflake.createStatement( {sqlText: V_FETCH_APPLICATION_NAME_SQ} ).execute();
+	
+		if(V_FETCH_APPLICATION_NAME_ST.next())
+		{
+			V_APPLICATION_NAME = V_FETCH_APPLICATION_NAME_ST.getColumnValue(1);
+		}
+		var V_SEQ = 0;
+		
+		var V_SEQ_SQ = `SELECT SEQ_AUDIT_`+ V_APPLICATION_NAME +`.NEXTVAL`;
+		
+		var V_SEQ_ST = snowflake.createStatement( {sqlText: V_SEQ_SQ} ).execute();
+		
+		if(V_SEQ_ST.next())
+		{
+			V_SEQ=V_SEQ_ST.getColumnValue(1);
+		}
+
+---------Inserting AUDIT_DTLS entry for status as R
+
+		var V_INS_AUDIT_DML = `INSERT INTO `+V_DATABASE_SCHEMA_NAME+`.k_project_name_AUDIT_DTLS(AUDIT_ID,APPLICATION_ID, BATCH_ID, SUBJECT_AREA_NAME, INTERFACE_NAME, TASK_NAME, TASK_TYPE, EXECUTION_START_TIME, EXECUTION_END_TIME, EXECUTION_TIME_SECS, EXECUTION_STATUS, EXECUTION_STATUS_DESC, ERROR_DESC, CREATED_DATE, CREATED_BY, LAST_UPDATE_DATE, LAST_UPDATE_BY)
+			VALUES (`+V_SEQ+`,` + V_APPLICATION_ID +`,` + V_BATCH_ID +`,''` + P_SUB_AREA_NM +`'',''`+ P_INTERFACE_NM +`'',''`+ P_TASK_NAME +`'',''`+ P_TASK_TYPE +`'',CURRENT_TIMESTAMP(),NULL,NULL, ''`+P_EXECUTION_STATUS +`'', ''`+ V_STAT_DESC + `'' ,NULL,CURRENT_TIMESTAMP(),''ETL_USER'', CURRENT_TIMESTAMP(),''ETL_USER'' )`;
+	
+		v_return_value += "\\n V_INS_AUDIT_DML: " + V_INS_AUDIT_DML;
+	
+		var V_INS_AUDIT_DML_ST = snowflake.createStatement( {sqlText: V_INS_AUDIT_DML} ).execute();
+		
+	}
+	if ( P_EXECUTION_STATUS != ''R'' )
+	{
+		var output_return_value=P_EXCEP_ERR.replace(/''/g,"''''");
+
+-------Updating AUDIT_DTLS with parametered P_EXCEP_ERR in case of C and F
+   
+		var V_UPD_ARCH_DML = `UPDATE `+V_DATABASE_SCHEMA_NAME+`.k_project_name_AUDIT_DTLS SET EXECUTION_END_TIME = CURRENT_TIMESTAMP(),EXECUTION_TIME_SECS = datediff(SECOND, EXECUTION_START_TIME, CURRENT_TIMESTAMP()),EXECUTION_STATUS = ''` + P_EXECUTION_STATUS +`'', EXECUTION_STATUS_DESC = ''` +V_STAT_DESC +`'', ERROR_DESC = DECODE(''` + output_return_value +`'',''undefined'',NULL,''` + output_return_value +`'') , LAST_UPDATE_DATE = CURRENT_TIMESTAMP()	WHERE UPPER(SUBJECT_AREA_NAME) = UPPER(''` + P_SUB_AREA_NM +`'') AND UPPER(TASK_NAME) = UPPER(''`+ P_TASK_NAME +`'') AND BATCH_ID = ` + V_BATCH_ID +` AND EXECUTION_STATUS = ''R''`;
+		
+		v_return_value += "\\n V_UPD_ARCH_DML: " + V_UPD_ARCH_DML;
+		
+		var V_UPD_ARCH_DML_ST = snowflake.createStatement( {sqlText: V_UPD_ARCH_DML} ).execute();
+
+	}
+
+	
+	snowflake.execute({ sqlText: "COMMIT;"}); 
+	
+	return "SUCCEEDED, Details: " +v_return_value;
+	
+	var check_flg=0;
+	
+	
+}
+catch (err)  
+{
+	snowflake.execute({ sqlText: "ROLLBACK;"});
+
+	var check_flg=1;
+
+	v_return_value = v_return_value+  "\\n  Failed: Code: " + err.code + "\\n  State: " + err.state;
+	
+    v_return_value = v_return_value+ "\\n  Message: " + err.message;
+	
+    v_return_value = v_return_value+ "\\nStack Trace:\\n" + err.stackTraceTxt;
+  
+}
+if(check_flg==0)
+{
+	return "Succeeded" + v_return_value;
+	
+}
+else
+{
+	throw "FAILED, Details:" + v_return_value;
+}
+';
---
+++ commom_components/data_validation/SP_k_project_name_BATCH_RUN_DTLS.sql
@@ -0,0 +1,179 @@
+CREATE OR REPLACE PROCEDURE k_database_name.k_schema_name.SP_k_project_name_BATCH_RUN_DTLS("P_APPLICATION_NAME" VARCHAR(200), "P_SUBJECT_AREA_NAME" VARCHAR(200), "P_STEP" VARCHAR(200))
+RETURNS VARCHAR(16777216)
+LANGUAGE JAVASCRIPT
+STRICT
+EXECUTE AS OWNER
+AS '
+try 
+{
+	
+    /* snowflake.execute({ sqlText: "Begin Transaction;"});*/
+	
+	var return_value="";
+    
+    var V_CONTROL_SCHEMA_NAME = "k_schema_name";
+    
+-------Selecting Param value from parameter table
+	
+    	var V_SQ = `SELECT PARAM_VALUE FROM `+V_CONTROL_SCHEMA_NAME+`.k_project_name_PARAMETER_TABLE WHERE PARAM_NAME = ''CONTROL'' AND APPLICATION_ID = (SELECT APPLICATION_ID FROM `+V_CONTROL_SCHEMA_NAME+`.k_project_name_APPLICATION_DTLS WHERE UPPER(APPLICATION_NAME) = UPPER(''`+ P_APPLICATION_NAME +`''))`;
+    
+    	var V_ST = snowflake.createStatement( {sqlText: V_SQ} ).execute();
+    
+    	V_ST.next();
+    
+    	var V_DATABASE_SCHEMA_NAME = V_ST.getColumnValue(1);
+
+-------If bach needs to created 
+	
+	if (P_STEP == ''BATCH_GEN'')
+	{
+		return_value +="\\n P_STEP: " + P_STEP ;
+	
+--------Selecting max batch value from batch table
+	
+		var V_SQ1 = `SELECT BATCH_ID, LOAD_STATUS FROM `+V_DATABASE_SCHEMA_NAME+`.k_project_name_BATCH_RUN_DTLS WHERE APPLICATION_ID = (SELECT APPLICATION_ID FROM `+V_DATABASE_SCHEMA_NAME+`.k_project_name_APPLICATION_DTLS WHERE UPPER(APPLICATION_NAME) = UPPER(''`+ P_APPLICATION_NAME +`'')) AND BATCH_ID = (SELECT MAX (BATCH_ID) FROM `+V_DATABASE_SCHEMA_NAME+`.k_project_name_BATCH_RUN_DTLS WHERE UPPER(SUBJECT_AREA_NAME) = UPPER(''`+ P_SUBJECT_AREA_NAME +`''))`;
+		
+		var V_ST1 = snowflake.createStatement( {sqlText: V_SQ1} ).execute();
+		
+		if(V_ST1.next())
+		{
+			var MAX_BATCH_ID_VALUE =V_ST1.getColumnValue(1);
+			
+			var PREV_LOAD_STATUS =V_ST1.getColumnValue(2);
+			
+			return_value += "\\n BATCH_ID_VALUE: " + MAX_BATCH_ID_VALUE;
+			
+			return_value += "\\n PREV_LOAD_STATUS: " + PREV_LOAD_STATUS;
+			
+--------Incrementing batch_id with batch_id+1
+			
+			var V_BATCH_ID = MAX_BATCH_ID_VALUE+1;
+		}
+		else
+		{
+			
+			var V_BATCH_ID=1;
+		}
+		
+--------For restartibility purpose, marking the batch as F
+		
+		var V_SQ2 = `UPDATE `+V_DATABASE_SCHEMA_NAME+`.k_project_name_BATCH_RUN_DTLS SET LOAD_END_TIME = CURRENT_TIMESTAMP(),LOAD_TIME_TAKEN_SECS = datediff(SECOND, LOAD_START_TIME, CURRENT_TIMESTAMP()), LOAD_STATUS=''F'' WHERE UPPER(SUBJECT_AREA_NAME)=UPPER(''`+ P_SUBJECT_AREA_NAME +`'') AND APPLICATION_ID = (SELECT APPLICATION_ID FROM `+V_DATABASE_SCHEMA_NAME+`.k_project_name_APPLICATION_DTLS WHERE UPPER(APPLICATION_NAME)=UPPER(''`+ P_APPLICATION_NAME +`'')) AND LOAD_STATUS=''R''`;
+		
+		return_value += "\\n UPD_BATCH_DTLS_SQL: "+ V_SQ2;
+		
+		var V_ST2 = snowflake.createStatement( {sqlText: V_SQ2} ).execute();
+		
+-------Feching respective application id from application table 
+		
+		var V_APPLICATION_ID_SQ = `SELECT APPLICATION_ID FROM `+V_DATABASE_SCHEMA_NAME+`.k_project_name_APPLICATION_DTLS WHERE UPPER(APPLICATION_NAME)= UPPER(''`+ P_APPLICATION_NAME +`'')`;
+		
+		var V_APPLICATION_ID_ST = snowflake.createStatement( {sqlText: V_APPLICATION_ID_SQ} ).execute();
+		
+		if(V_APPLICATION_ID_ST.next())
+		{
+			var V_APPLICATION_ID =V_APPLICATION_ID_ST.getColumnValue(1);
+			
+			return_value += "\\n APPLICATION_ID: " + V_APPLICATION_ID;
+		}
+		if(V_APPLICATION_ID)
+		{
+
+----------Inserting new batch entry 
+	
+			var V_SQ3 = `INSERT INTO `+V_DATABASE_SCHEMA_NAME+`.k_project_name_BATCH_RUN_DTLS (BATCH_ID,APPLICATION_ID,SUBJECT_AREA_NAME,EXECUTION_DATE,LOAD_START_TIME,LOAD_STATUS) VALUES (`+V_BATCH_ID+`,`+ V_APPLICATION_ID +`,''`+ P_SUBJECT_AREA_NAME +`'',CURRENT_DATE(),CURRENT_TIMESTAMP(),''R'')`;
+			 
+			return_value += "\\n V_INS_BATCH_DTLS: " + V_SQ3;
+			
+			var V_ST3 = snowflake.createStatement( {sqlText: V_SQ3} ).execute();
+
+// To create the sequence internally when ever the batch is inserted as in "R" state.
+            
+             var V_SQ6 = `CALL "SP_k_project_name_SEQUENCE_GEN" (''`+P_APPLICATION_NAME+`'',''OPEN_SEQ'')`;
+             
+             var V_ST6 = snowflake.createStatement( {sqlText: V_SQ6} ).execute();
+             
+             V_ST6.next();
+             
+             return_value += ''\\n SEQUENCE_STATUS: ''+V_ST6.getColumnValue(1);
+
+//
+             			
+		}
+		else
+		{
+			return_value += ''\\n  GIVEN APPLICATION_NAME IS INVALID \\n'';
+			
+			throw return_value;
+		}	
+	}
+	
+-------If batch needs to be closed
+
+	if (P_STEP == ''BATCH_CLS'')
+	{
+		var V_SQ4 = `SELECT BATCH_ID FROM `+V_DATABASE_SCHEMA_NAME+`.k_project_name_BATCH_RUN_DTLS WHERE UPPER(SUBJECT_AREA_NAME) = UPPER(''`+ P_SUBJECT_AREA_NAME +`'') AND LOAD_STATUS = ''R''`;
+		
+		var V_ST4 = snowflake.createStatement( {sqlText: V_SQ4} ).execute();
+		if(V_ST4.next())
+		{
+			V_CURR_BATCH = V_ST4.getColumnValue(1);
+
+// To drop the sequence internally when ever the batch is closed as in "C" state.
+            
+            var V_SQ7 = `CALL "SP_k_project_name_SEQUENCE_GEN" (''`+P_APPLICATION_NAME+`'',''CLOSE_SEQ'')`;
+            
+            var V_ST7 = snowflake.createStatement( {sqlText: V_SQ7} ).execute();
+            
+            V_ST7.next();
+            
+            return_value += ''\\n SEQUENCE_STATUS: ''+V_ST7.getColumnValue(1);
+
+//
+
+------------Marking batch as Complete C
+            
+			var V_SQ5 = `UPDATE `+V_DATABASE_SCHEMA_NAME+`.k_project_name_BATCH_RUN_DTLS SET LOAD_END_TIME = CURRENT_TIMESTAMP() ,LOAD_TIME_TAKEN_SECS = datediff (SECOND,LOAD_START_TIME, CURRENT_TIMESTAMP()), LOAD_STATUS = ''C'' WHERE UPPER(SUBJECT_AREA_NAME) = UPPER(''`+ P_SUBJECT_AREA_NAME +`'') AND LOAD_STATUS = ''R''AND BATCH_ID = `+ V_CURR_BATCH +``;
+			
+            return_value += "\\n V_UPD_BATCH_DTLS2: " + V_SQ5;
+            
+			var V_ST5 = snowflake.createStatement( {sqlText: V_SQ5} ).execute();
+            
+           
+		}
+		else
+		{
+			return_value+=" NO BATCH IS OPEN FOR THIS INTERFACE. HENCE THE BATCH CAN NOT BE CLOSED ";
+			
+			throw return_value;
+		}
+	}
+	
+	/* snowflake.execute({ sqlText: "COMMIT;"});  */
+	
+	return "SUCCEEDED, Details: " +return_value;
+	
+	var check_flg=0;
+}
+catch (err)  
+{
+	/* snowflake.execute({ sqlText: "ROLLBACK;"});*/
+	
+	var check_flg=1;
+	
+	return_value +=  "\\n  Failed: Code: " + err.code + "\\n  State: " + err.state;
+	
+	return_value += "\\n  Message: " + err.message;
+	
+	return_value += "\\nStack Trace:\\n" + err.stackTraceTxt;
+  
+}
+if(check_flg==0)
+{
+	return "Succeeded" + return_value;
+	
+}
+else
+{
+	throw "FAILED, Details:" + return_value;
+}
+';
